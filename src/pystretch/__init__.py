cc6bbdf3e02094c583a91708bf4f9fb7886a9504
Gregorio Andrea  Giudici <gregorio.giudici@unitn.it>
2024-09-19 12:23:33 +0200

    Fix import error

--- src/pystretch/__init__.py
+++ src/pystretch/__init__.py
@@ -2,4 +2,4 @@
 __doc__ = "A Python Wrapprer of the Signalsmith Stretch C++ library for pitch and time stretching"
 __version__ = "0.1.0"
 
-from . import SignalsmithStretch
+from . import Signalsmith
