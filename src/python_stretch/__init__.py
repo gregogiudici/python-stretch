a73ff361bd0a4239823c10e0312021d1a41e537a
Gregorio Andrea Giudici <gregorio.giudici@unitn.it>
2025-02-13 17:39:20 +0100

    Add stubs and optimize stretch compilation

--- src/python_stretch/__init__.py
+++ src/python_stretch/__init__.py
@@ -1,5 +1,5 @@
 # This file is required to make Python treat the directories as containing packages
 __doc__ = "A simple python library for pitch shifting and time stretching"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from . import Signalsmith
